554e8e452c9bb32d86987e2a2af506e3e9498006
blinkous <saradhani1@gmail.com>
2018-09-18 17:46:17 -0400

    Design changes

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.stopwatch;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**Animates the background colors of the main screen background_animation in res/animator**/
+        RelativeLayout myCon = findViewById(R.id.mainlayout);
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(this,
+                R.animator.background_animation);
+        set.setTarget(myCon);
+        set.start();
     }
 
     public void startStopwatch(View view){
@@ -21,15 +32,19 @@ public class MainActivity extends AppCompatActivity {
             Chronometer chronometer = findViewById(R.id.myChronometer);
             chronometer.start();
             Button startButton = findViewById(R.id.startStopwatch);
-            startButton.setText("Stop Stopwatch");
+            startButton.setText(getResources().getString(R.string.stop_stopwatch));
             isTimerRunning = true;
         }
         else{
             Chronometer chronometer = findViewById(R.id.myChronometer);
             chronometer.stop();
             Button startButton = findViewById(R.id.startStopwatch);
-            startButton.setText("Start Stopwatch");
+            startButton.setText(getResources().getString(R.string.start_stopwatch));
             isTimerRunning = false;
         }
+    }
+
+    public void restartStopwatch(View view){
+        Chronometer chronometer = findViewById(R.id.myChronometer);
     }
 }
